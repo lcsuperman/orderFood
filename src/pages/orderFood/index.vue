8edb523a8efb49968e102b5febbfa9740d733527
wangyu <wangyu@insdo.cn>
2020-05-21 19:43:41 +0800

    滚动效果

--- src/pages/orderFood/index.vue
+++ src/pages/orderFood/index.vue
@@ -2,15 +2,27 @@
      <div class="orderFood">
        <Header></Header>
        <div class="orderFood-container">
-
+          <div class="orderFood-content">
+             <span>1111111111111111111</span>
+          </div>
        </div>
      </div>
 </template>
 <script>
   import Header from '../../components/header/index.vue'
+  import BScroll from 'better-scroll'
   export default{
     components:{
       Header
+    },
+    mounted(){
+      new BScroll('.orderFood-container', {
+        click: true,
+        bounce: {
+          top: true,
+          bottom: false,
+        }
+      })
     }
   }
 </script>
@@ -18,7 +30,11 @@
   .orderFood
      width 100%
      height 100%
+     background-color #fff
      .orderFood-container
        height 100%
-       background-color #7e8c8d
+       .orderFood-content
+         background-color #7e8c8d
+         height 100%
+         padding-bottom 1px
 </style>
